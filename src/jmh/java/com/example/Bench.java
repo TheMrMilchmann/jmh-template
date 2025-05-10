c5e9fe4b3f15029d573526870c5e19ac58b6cf4d
Leon Linhart <themrmilchmann@gmail.com>
2025-05-10 09:03:25 +0200

    feat: add dummy code file

---
+++ src/jmh/java/com/example/Bench.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class Bench {
+
+    @Benchmark
+    public void bench() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+}
